1cfbeb8b9ff414d2b01c602a5857867a7242f9cf
zzack43 <couto.joseafonso@gmail.com>
2026-05-28 18:58:35 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pokedex-ai-v1';
+const ASSETS = [
+  'index3.2.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
